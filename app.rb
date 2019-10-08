e2ad76b5cb6339ce7a085756efa9a58cdbc3116b
Urfan Guliyev <urfan196@yahoo.com>
2019-10-08 00:29:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,4 +20,21 @@ class App < Sinatra::Base
     [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]].join(" ") + '.'
   end
 
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when 'add'
+      sum = params[:number1].to_i + params[:number2].to_i
+      sum.to_s
+    when 'subtract'
+      subtract = params[:number2].to_i - params[:number1].to_i
+      subtract.to_s
+    when 'multiply'
+      product = params[:number1].to_i * params[:number2].to_i
+      product.to_s
+    when 'divide'
+      divide = params[:number1].to_i / params[:number2].to_i
+      divide.to_s
+    end
+  end
+
 end

04d5b345339137043bb1e41334cadb5bdfa233e7
Urfan Guliyev <urfan196@yahoo.com>
2019-10-08 00:18:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,16 +13,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @num = params[:number].to_i
-    @p = params[:phrase]
-    @string = []
-    @num * @p
-    #  params[:phrase] * params[:number].to_i
-    # @num.times do
-    # "#{@p}"
-    # end
+  params[:phrase] * params[:number].to_i
   end
 
-
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]].join(" ") + '.'
+  end
 
 end
